d28b9722ffb77561011b9e863d35b41b16890764
Theo Carney <theoluyi@gmail.com>
2019-12-01 19:55:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ end
 def my_hash_creator(key, value)
   h = {}
   h[key] = value 
-
+  h
   #FAIL1 h = {:[key] => value}
   # return a hash that includes the key and value parameters passed into this method
 end
