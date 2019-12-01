0a856a7d85e4451462bac062c88cab9d10336b7f
Theo Carney <theoluyi@gmail.com>
2019-12-01 19:48:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  h = {name: value}
+  h = {key: value}
   # return a hash that includes the key and value parameters passed into this method
 end
 
